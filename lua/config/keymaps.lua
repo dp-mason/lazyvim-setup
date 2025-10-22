f57433d7b73a3e288a867d6b8315f5439cb9cffa
dp-mason <davidpattenmason@gmail.com>
2025-10-22 01:03:55 -0500

    add currently disabled keymaps that will disable the arrow keys for navigation, I am not ready yet

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- I can't do this shit yet, but eventually...
+-- map("n", "<Up>", "<Nop>", { desc = "Disabled" })
+-- map("n", "<Down>", "<Nop>", { desc = "Disabled" })
+-- map("n", "<Left>", "<Nop>", { desc = "Disabled" })
+-- map("n", "<Right>", "<Nop>", { desc = "Disabled" })
+--
+-- map("v", "<Up>", "<Nop>", { desc = "Disabled" })
+-- map("v", "<Down>", "<Nop>", { desc = "Disabled" })
+-- map("v", "<Left>", "<Nop>", { desc = "Disabled" })
+-- map("v", "<Right>", "<Nop>", { desc = "Disabled" })
